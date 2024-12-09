5c3ff52b88c8a3dcd93d8b821040818f545800c5
Pierluigi <pigi.ped@hotmail.it>
2024-12-09 16:41:59 +0100

    added flying slime, removed flipped slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,17 +4,18 @@ const SPEED = 30
 
 var direction = 1
 
-@onready var ray_cast_right: RayCast2D = $RayCastRight
-@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_castright: RayCast2D = $RayCastRight
+@onready var ray_castleft: RayCast2D = $RayCastLeft
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if ray_cast_right.is_colliding():
+	if ray_castright.is_colliding():
 		direction = -1	
 		animated_sprite.flip_h = true
 		
-	if(ray_cast_left).is_colliding():
+	if(ray_castleft).is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
